d3ae325466b3454ba7818242a01c43a9569cc9cd
Markus Obereisenbuchner <halbesbyte@googlemail.com>
2021-02-06 22:27:45 +0100

    add manual build script for pyinstaller bootloader

---
+++ build_and_install_pyinstaller.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = "Stop"
+
+# configuration area
+$PYINSTALLER_VERSION = 'v4.2'
+
+# pyinstaller build process fails on long paths
+pushd ~
+
+# download pyinstaller source
+$source = "https://github.com/pyinstaller/pyinstaller/archive/$PYINSTALLER_VERSION.zip"
+$destination = "pyinstaller.zip"
+$ProgressPreference = "SilentlyContinue" # Significantly speeds up Invoke-WebRequest
+Invoke-WebRequest $source -OutFile $destination
+
+# unzip pyinstaller source
+7z x $destination
+rm $destination
+mv pyinstaller-*.*/ pyinstaller_src/
+
+# build pyinstaller bootloader
+pushd pyinstaller_src/bootloader
+python ./waf all
+popd
+
+# install pyinstaller python package
+pushd pyinstaller_src
+python setup.py install
+python -m pip show pyinstaller # show version
+popd
+rm -r -fo pyinstaller_src/
+
+popd
